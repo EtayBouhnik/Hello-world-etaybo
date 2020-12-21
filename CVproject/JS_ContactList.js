3983f702aeab6b4b2a7bbcdac229a3dd4bad161f
Etay <etaybo@post.bgu.ac.il>
2020-12-21 17:03:43 +0200

    part7

---
+++ CVproject/JS_ContactList.js
@@ -0,0 +1,22 @@
+fetch("https://reqres.in/api/users?page=2").then(response => response.json()).then(responseJSON => createUsersList(responseJSON.data)).catch(err => console.log(err));
+
+function createUsersList(users){
+    console.log(users);
+    const curr = document.querySelector("main");
+    for (let user of users){
+        const section = document.createElement("section");
+        section.innerHTML = `
+        <ul class="columns" data-columns="2">
+        <div>
+        <img src ="${user.avatar}" alt="profile picture"/>
+        <br>
+        <span>${user.first_name}   ${user.last_name}</span>
+            <br>
+            ${user.email}
+         <br><br>
+        </div>
+        </ul>
+        `;
+        curr.appendChild(section);
+    }
+}
